f8051d1b9da7a410792140f9691471aaa9bfc94a
Vitor Cavalcante <dev.vitoralves2c@outlook.com>
2024-01-19 13:44:58 -0300

    Day 16 - Coffee Machine OOP

--- day 16/main.py
+++ day 16/main.py
@@ -4,9 +4,9 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 # Creating instances of the Menu, CoffeeMaker, and MoneyMachine classes
-machine_menu = Menu()
+menu = Menu()
 coffee_machine = CoffeeMaker()
-register = MoneyMachine()
+money_machine = MoneyMachine()
 
 # Variable to control the main loop
 is_on = True
@@ -14,7 +14,8 @@ is_on = True
 # Main program loop
 while is_on:
     # Prompting the user to input their order
-    order = input("What would you like? (espresso/latte/cappuccino): ")
+    options = menu.get_items()
+    order = input(f"What would you like? ({options}): ")
 
     # Checking if the user wants to turn off the coffee machine
     if order == 'off':
@@ -22,13 +23,14 @@ while is_on:
     # Checking if the user wants a report of the coffee machine's resources
     elif order == 'report':
         coffee_machine.report()
+        money_machine.report()
     else:
         # Finding the drink object based on the user's order
-        drink = machine_menu.find_drink(order)
+        drink = menu.find_drink(order)
 
         # Checking if there are enough resources to make the selected drink
         if coffee_machine.is_resource_sufficient(drink):
             # Checking if the user has made a successful payment for the drink
-            if register.make_payment(drink.cost):
+            if money_machine.make_payment(drink.cost):
                 # Making the coffee if both resource and payment conditions are met
                 coffee_machine.make_coffee(drink)
