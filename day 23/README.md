22cb39c822c36fd96568bafd2a628835824ba641
Vitor Cavalcante <dev.vitoralves2c@outlook.com>
2024-01-23 21:03:48 -0300

    Day 22 and 23 - Pong Game and Turtle Crossing Game

---
+++ day 23/README.md
@@ -0,0 +1,7 @@
+# Turtle Crossing Game
+
+A Python implementation of the classic Turtle Crossing game using the Turtle graphics library. Guide the turtle safely across the road while avoiding oncoming cars. The game features increasing difficulty levels and a scoreboard to track your progress.
+
+## Controls
+
+Move using the "W" key in your keyboard
---
+++ day 23/main.py
@@ -0,0 +1,38 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(fun=player.go_up, key="w")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if player.distance(car) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+
+    if player.ycor() == 280:
+        player.refresh()
+        scoreboard.next_level()
+        scoreboard.update_scoreboard()
+        car_manager.increase_speed()
+
+
+screen.exitonclick()
