1a2c5412817b47585a12c5244c92026461f2ea9a
Vitor Cavalcante <dev.vitoralves2c@outlook.com>
2024-02-01 23:20:35 -0300

    Day 29 - MyPass Password Generator

--- day 29/main.py
+++ day 29/main.py
@@ -1,28 +1,52 @@
 from tkinter import *
 from tkinter import messagebox
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# Password Generator Project
+from random import randint, shuffle, choice
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q',
+               'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = randint(8, 10)
+    nr_symbols = randint(2, 4)
+    nr_numbers = randint(2, 4)
+
+    password_letters = [choice(letters) for _ in range(nr_letters)]
+    password_symbols = [choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    pswd = "".join(password_list)
+    password_entry.insert(0, f"{pswd}")
+    pyperclip.copy(pswd)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save_password():
-
     website = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
-    is_empty = False
-
-    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: "
-                                                  f"\nEmail: {username} "
-                                                  f"\nPassword: {password} "
-                                                  f"\nIs is okay to save?")
 
     if len(website) == 0 or len(username) == 0 or len(password) == 0:
-        is_empty = True
-
-    if is_empty:
-        messagebox.showinfo(title="Please don't leave any fields empty")
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty")
     else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: "
+                                                              f"\nEmail: {username} "
+                                                              f"\nPassword: {password} "
+                                                              f"\nIs is okay to save?")
+
         if is_ok:
             with open("passwords.txt", "a") as data:
                 data.write(f"{website}; ")
@@ -75,7 +99,7 @@ password_entry = Entry(width=21)
 password_entry.grid(column=2, row=4)
 
 # Generate Password
-button = Button(text="Generate Password")
+button = Button(text="Generate Password", command=generate_password)
 button.grid(column=3, row=4)
 
 # Save Password
